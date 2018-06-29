a21f9627601021ffed9518763399de20ff906bc5
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-06-29 18:25:16 +0200

    Application container

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import applications from './Applications';
+import Application from './Application';
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
+  flex-direction: column;
+  position: absolute;
+  width: 50%;
+  top: 0;
+  left: 50%;
 `;
 
+const Title = styled.h1`
+  text-align: center;
+`;
+const ApplicationContainer = styled.div``;
 class Portfolio extends Component {
   constructor() {
     super();
+    this.state = {
+      applications: applications
+    };
   }
   render() {
     return (
       <Container>
+        <Title>My (D)-Applications</Title>
 
+        <ApplicationContainer>
+          {this.state.applications.map(app => {
+            return <Application app={app}>{app.description}</Application>;
+          })}
+        </ApplicationContainer>
       </Container>
     );
   }
