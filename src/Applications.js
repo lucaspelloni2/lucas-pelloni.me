63bd5b5a7342a3add12b2015a4c595d15ad2855b
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-06-29 18:24:57 +0200

    applications file

---
+++ src/Applications.js
@@ -0,0 +1,18 @@
+const applications = [
+  {
+    name: 'Dapp Tic-Tac-Toe',
+    link: 'http://www.tictactoe.bet/',
+    img: 'tictactoe.png',
+    description:
+      'aspfansf as fahisfi asfoias f asfo asfashif aipsf asfipaspf apfpasf asfpas fa sfpihasfp asf aaspf asjf pasfp asf',
+    github: 'https://github.com/lucaspelloni2/tic-tac-toe/'
+  },
+  {
+    name: '',
+    link: '',
+    description: '',
+    github: ''
+  }
+];
+
+export default applications;
